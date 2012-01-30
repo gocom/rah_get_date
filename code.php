c7783c239d261c61cafa2cfc6eb2c7e81e6dd795
Jukka Svahn <web@rahinaa.biz>
2012-01-31 00:08:12 +0200

    Adding v0.2 from SVN.

--- code.php
+++ code.php
@@ -1,7 +1,7 @@
 <?php	##################
 	#
 	#	rah_get_date-plugin for Textpattern
-	#	version 0.1
+	#	version 0.2
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
@@ -9,10 +9,52 @@
 
 	function rah_get_date($atts) {
 		extract(lAtts(array(
-			'format' => '%Y %B'
+			'format' => '%Y %B',
+			'format_year' => '',
+			'format_month' => '',
+			'format_date' => '',
+			'format_hour' => '',
+			'format_minute' => '',
+			'format_second' => '',
+			'format_now' => '',
+			'now_condition' => '%Y-%m-%d',
+			'date' => ''
 		),$atts));
-		$date = (gps('month')) ? gps('month') : gps('date');
-		$out = ($date) ? safe_strftime($format,strtotime($date)) : '';
-		return $out;
+		
+		$date = trim($date ? $date : (gps('month') ? gps('month') : gps('date')));
+		
+		if(!$date || ($valid = strtotime($date)) == -1 || $valid === false)
+			return;
+		
+		$udate = safe_strtotime($date);
+		
+		if(
+			$format_now && $now_condition &&
+			safe_strftime($now_condition, safe_strtotime('now')) === 
+			safe_strftime($now_condition, $udate)
+		)
+			$format = $format_now;
+		else {
+		
+			$ds = 
+				substr_count($date, '-') + 
+				substr_count($date, ' ') + 
+				substr_count($date, ':');
+		
+			$formats = 
+				array(
+					0 => 'year',
+					1 => 'month',
+					2 => 'date',
+					3 => 'hour',
+					4 => 'minute',
+					5 => 'second'
+				);
+		
+			if(isset($formats[$ds]) && isset($atts['format_'.$formats[$ds]]))
+				$format = $atts['format_'.$formats[$ds]];
+		}
+		
+		return $format ? safe_strftime($format, $udate) : '';
 	}
 ?>
